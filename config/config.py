263c9cd17c0a4e5c509af1a0dcfb59bec2cb5e8e
Darren Jones <djaychela@gmail.com>
2018-03-15 20:42:18 +0000

    Detail View for Year groups, more consistency in function names

--- config/config.py
+++
@@ -1,7 +0,0 @@
-# class related info
-class_attributes_list = ['id','class_strand','class_year','class_teacher', 'active']
-
-# pupils related info
-pupils_attributes_dict = {'id':'id', 'first_name': 'first_name', 'last_name': 'last_name', 'class_id': 'class_id'}
-# user related info
-user_attributes_list = ['id', 'username', 'password_hash', 'cap_view', 'cap_edit']
